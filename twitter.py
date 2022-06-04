f192e3468003034346e5f22c6b9af924e0522d7d
testacc93 <84958260+testacc93@users.noreply.github.com>
2022-06-04 16:36:09 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -47,6 +47,7 @@ for j in range(0, 10000):
         
         tweet = items + link + hash_tags + str(datetime.datetime.now())
         print(len(tweet))
+        sleep(600)
         
 
         if len(tweet) < 280:
@@ -72,4 +73,4 @@ for j in range(0, 10000):
 
             except:
                 pass
-        sleep(600)
+        
